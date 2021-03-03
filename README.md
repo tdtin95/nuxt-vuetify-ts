16dd5208766c7fc8e4bede29b93e5d72696c8bdc
Tin Tran <tdtin95@gmail.com>
2021-03-03 23:15:09 +0700

    slot with dialog

---
+++ README.md
@@ -0,0 +1,20 @@
+# nuxt-vuetify-ts
+Project demo to try out nuxt-ts and vuetify
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="text-center">
+    <Dialog>
+      <template #default="slotProps">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="slotProps.attrs"
+          v-on="slotProps.on"
+          @click="handle(slotProps.on, slotProps.attrs)"
+        >
+          Click Me
+        </v-btn>
+      </template>
+
+    </Dialog>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Dialog from "~/components/dialog/Dialog.vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      dialog: false,
+    }
+  },
+  components: {
+    Dialog,
+    Logo,
+    VuetifyLogo
+  },
+  methods: {
+    handle(e: Function, atts : Object): void {
+      console.log(e);
+      console.table (atts);
+    }
+  }
+})
+</script>
